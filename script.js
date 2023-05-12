f5bface818f6bf01951646e779b52fb17362ad21
GabrielBaiaDias <102996530+GabrielBaiaDias@users.noreply.github.com>
2023-05-12 19:12:02 -0300

    melhorias

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+var resultado = [];
 
-function Formatar() {
-    debugger
-    var text1 = document.getElementById("areaText1");
-    var text2 = document.getElementById("areaText2");
-    var palavras = [];
+function Remover() {
+  debugger
+  var text1 = document.getElementById("areaText1");
+  var text2 = document.getElementById("areaText2");
+  var palavras = [];
 
 
-    palavras = text1.value.split(' ').filter(x => x != "");
+  palavras = text1.textContent.split(/\s+/g);
 
-    text2.innerText = palavras.join(" ").trim();
+  text2.textContent = palavras.join(' ');
 }
+
+function Aplicar() {
+  var editor = document.getElementById("areaText2");
+  var colorPicker = document.getElementById("cor");
+  
+  if (document.queryCommandSupported('styleWithCSS')) {
+    document.execCommand('styleWithCSS', false, true);
+  }
+  
+  document.execCommand('foreColor', false, colorPicker.value);
+}
+
+// function Formatar() {
+//   document.execCommand(command, false, null);
+// }
